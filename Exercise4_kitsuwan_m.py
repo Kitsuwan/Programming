293b69a84ecd05dffbb8c6bcbbbe2d0ffd1b9168
Kitsuwan <52350459+Kitsuwan@users.noreply.github.com>
2019-08-03 19:07:21 +0700

    Add files via upload

---
+++ Exercise4_kitsuwan_m.py
@@ -0,0 +1,13 @@
+f="Foundation English:"
+FE=75
+g="General Business:"
+GB=70
+i="Introduction to Computer systems:"
+ICS=65
+C="Computer Programming:"
+CR=50
+print("-----your Score-----")
+print(f,FE)
+print(g,GB)
+print(i,ICS)
+print(C,CR)
